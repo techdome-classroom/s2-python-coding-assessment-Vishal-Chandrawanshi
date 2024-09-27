78006af40bfcf8559a1268ec8552788df6bee409
Vishal-Chandrawanshi <vishalchandrawanshi888@gmail.com>
2024-09-27 14:30:38 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,13 +1,27 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+def isValid(s: str) -> bool:
+
+    bracket_map = {')': '(', '}': '{', ']': '['}
+    stack = []
+    
+    for char in s:
+        if char in bracket_map.values():  
+            stack.append(char)
+        elif char in bracket_map.keys(): 
+            
+            if stack and stack[-1] == bracket_map[char]:
+                stack.pop() 
+            else:
+                return False  
+    
+   
+    return len(stack) == 0
 
 
-
+print(isValid("()"))          
+print(isValid("()[]{}"))      
+print(isValid("(]"))          
+print(isValid("([)]"))        
+print(isValid("{[]}"))       
 
 
 
